b7007f6e4d1f6a4ac538e95bc74a56eefd491585
bernardo <bernardo@socarrao.com.br>
2025-07-17 09:07:02 -0300

    crete migrations

--- db/migrations/20250715114238_create-meals.ts
+++ db/migrations/20250715114238_create-meals.ts
@@ -1,10 +1,25 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('meals', (table) => {
+        table.uuid('id').primary,
+        table.string('name', 255).notNullable(),
+        table.text('description')
+        table.date('date').notNullable()
+        table.time('hour').notNullable()
+        table
+          .integer("userId")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("users");
+        table.timestamps(true, true);
+        table.timestamp('deleted_at').nullable();
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('meals')
 }
 
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
 import fastify from "fastify";
+import { knex } from "./database";
 
 
 export const app = fastify()
+
+app.get('/test', async ()=> {
+    const test = await knex('sqlite_schema').select('*')
+    return test
+})

5061df88fb172fcec78d56e28820ef1a8b2e14be
bernardo <bernardo@socarrao.com.br>
2025-07-28 21:15:23 -0300

    implementing meals endpoints

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,5 +1,4 @@
 import { FastifyInstance } from "fastify";
-import { request } from "http";
 import z from "zod";
 import { validateWithZod } from "../utils/validateWithZod";
 import { checkSessionIdExists } from "../middlewares/checkSessionIdExists";
@@ -71,4 +70,127 @@ export async function mealsRoutes(app: FastifyInstance){
         return { meals }
       }
     );
+
+    app.get(
+      "/:id",
+      { preHandler: [checkSessionIdExists] },
+      async (request, reply) => {
+        const getMealsRouteSchema = z.object({
+          id: z.string()
+        })
+
+        const { id } = validateWithZod(getMealsRouteSchema, request.params)
+
+        const { sessionId } = request.cookies;
+
+        const meal = await knex("Meals").where({ id }).first()
+
+        if(!meal) throw new AppError("Meal Not Found", 404)
+
+        const user = await knex("Users").where({ sessionId }).first();
+
+        if (!user) throw new AppError("User not Found", 404);
+
+        if (user.id !== meal.userId) {
+          throw new AppError("The user does not own the meal", 403);
+        }
+
+        return { meal }
+      }
+    );
+
+    app.delete(
+      "/:id",
+      { preHandler: [checkSessionIdExists] },
+      async (request, reply) => {
+        const getMealsRouteSchema = z.object({
+          id: z.string()
+        })
+
+        const { id } = validateWithZod(getMealsRouteSchema, request.params)
+
+        const { sessionId } = request.cookies;
+
+        const meal = await knex("Meals").where({ id }).first()
+
+        if(!meal) throw new AppError("Meal Not Found", 404)
+
+        const user = await knex("Users").where({ sessionId }).first()
+
+        if (!user) throw new AppError("User not Found", 404);
+
+        if(user.id !== meal.userId) {
+          throw new AppError("The user does not own the meal", 403)
+        }
+
+        await knex("Meals").where({ id }).update({ deleted_at: knex.fn.now() })
+
+        return reply.status(204).send()
+      }
+    );
+
+    app.put(
+      "/:id",
+      { preHandler: [checkSessionIdExists] },
+      async (request, reply) => {
+        const getMealsRouteSchema = z.object({
+          id: z.string()
+        })
+
+        const { id } = validateWithZod(getMealsRouteSchema, request.params);
+
+        const meal = await knex("Meals").where({ id }).first();
+
+        if(!meal) throw new AppError("Meal Not Found", 404);
+
+        const { sessionId } = request.cookies;
+
+        const user = await knex("Users").where({ sessionId }).first()
+
+        if (!user) throw new AppError("User not Found", 404);
+
+        if(user.id !== meal.userId) {
+          throw new AppError("The user does not own the meal", 403)
+        }
+
+        const updateMealBodySchema = z.object({
+          name: z.string(),
+          description: z.string(),
+          date: z.string().refine(
+            (val) => {
+              return /^\d{4}-\d{2}-\d{2}$/.test(val) && !isNaN(Date.parse(val));
+            },
+            {
+              message: "Date must be in YYYY-MM-DD format",
+            }
+          ),
+          hour: z.string().regex(/^([01]\d|2[0-3]):[0-5]\d$/, {
+            message: "Hour must be in format HH:mm",
+          }),
+          isOnDiet: z.boolean(),
+        });
+
+          const { name, description, date, hour, isOnDiet } = validateWithZod(
+            updateMealBodySchema,
+            request.body
+          );
+
+          await knex("Meals")
+            .where({ id })
+            .update({ name, description, date, hour, isOnDiet, updated_at: knex.fn.now() });
+
+          return reply.status(204).send()
+      }
+    );
+
+    app.get(
+      "/metrics",
+      { preHandler: [checkSessionIdExists] },
+      async (request, reply) => {}
+    );
+
+
+
+
+    
 }
